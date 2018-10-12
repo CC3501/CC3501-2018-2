14c2a0bd1f609facc04cb05b0d07913ccbab245b
ppizarror <pablo.pizarro@ing.uchile.cl>
2018-10-12 06:23:58 +0100

    Agrega auxiliar 4

---
+++ aux 4/triangulo.py
@@ -0,0 +1,95 @@
+# coding=utf-8
+"""
+Crea archivo ejemplo
+"""
+
+# Carga las librerías
+from PyOpenGLtoolbox.glpython import *
+from PyOpenGLtoolbox.opengl_lib import *
+from PyOpenGLtoolbox.camera import *
+from PyOpenGLtoolbox.utils import create_axes
+
+# Constantes
+AXES_LENGTH = 3
+CAMERA_PHI = 45
+CAMERA_RAD = 5
+CAMERA_ROT_VEL = 2.5
+CAMERA_THETA = 56
+FPS = 60
+WINDOW_SIZE = [800, 600]
+
+# Inicia la ventana
+init_pygame(WINDOW_SIZE[0], WINDOW_SIZE[1], 'Triangulo', centered_window=True)
+init_gl(transparency=False, materialcolor=False, normalized=True, perspectivecorr=True, antialiasing=True,
+        depth=True, smooth=True, verbose=True, version=True)
+reshape(*WINDOW_SIZE, nearplane=1)
+clock = pygame.time.Clock()
+
+# Crea objetos
+axis = create_axes(AXES_LENGTH)
+camera = CameraR(CAMERA_RAD, CAMERA_PHI, CAMERA_THETA)
+camera.set_r_vel(0.5)  # Velocidad radial
+
+# Crea el triangulo
+triangulo = glGenLists(1)  # Inicia una lista
+glNewList(triangulo, GL_COMPILE)
+glBegin(GL_POLYGON)  # Inicia una figura
+glColor3f(1, 0, 0)
+glVertex3f(-0.6, -0.75, 0.5)
+glColor3f(0, 1, 0)
+glVertex3f(0.6, -0.75, 0)
+glColor3f(0, 0, 1)
+glVertex3f(0, 0.75, 0)
+glEnd()  # Termina la figura
+glEndList()  # Cierra la lista
+
+# Muestra ayuda
+print('Rota eje X con W/S')
+print('Rota eje Y con A/D')
+print('Rota eje Z con Q/E')
+print('Zoom +/- con N/M')
+
+# Main loop
+while True:
+    clock.tick(FPS)
+    clear_buffer()
+    camera.place()
+
+    # Dibuja la luz
+    glCallList(axis)
+    glCallList(triangulo)
+
+    # Chequea eventos
+    for event in pygame.event.get():
+        if event.type == QUIT or (event.type == KEYDOWN and event.key == K_ESCAPE):  # Cierra la app
+            exit()
+
+    # Obtiene teclas presionadas
+    keys = pygame.key.get_pressed()
+
+    # Rota camara eje x
+    if keys[K_w]:
+        camera.rotate_x(CAMERA_ROT_VEL)
+    elif keys[K_s]:
+        camera.rotate_x(-CAMERA_ROT_VEL)
+
+    # Rota camara eje y
+    if keys[K_a]:
+        camera.rotate_y(-CAMERA_ROT_VEL)
+    elif keys[K_d]:
+        camera.rotate_y(CAMERA_ROT_VEL)
+
+    # Rota camara eje z
+    if keys[K_q]:
+        camera.rotate_z(-CAMERA_ROT_VEL)
+    elif keys[K_e]:
+        camera.rotate_z(CAMERA_ROT_VEL)
+
+    # Acerca / aleja la camara
+    if keys[K_n]:
+        camera.close()
+    elif keys[K_m]:
+        camera.far()
+
+    # Vuelca el contenido
+    pygame.display.flip()
