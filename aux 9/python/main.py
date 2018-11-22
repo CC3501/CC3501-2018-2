7cab8f217192fabbed4169490421eba0e7961b0d
ppizarror <pablo.pizarro@ing.uchile.cl>
2018-11-22 22:09:41 +0000

    Agrega principales archivos de python

---
+++ aux 9/python/main.py
@@ -0,0 +1,12 @@
+"""
+Código principal del problema. Instancia un plano y una pokebola.
+Esta rebotará siguiendo alguna dirección y una velocidad inicial.
+
+@author Pablo Pizarro R. @ppizarror
+@date 2018
+@license MIT
+"""
+
+# Importación de librerías
+from PyOpenGLtoolbox import *
+from pokebola import Pokebola
---
+++ aux 9/python/pokebola.py
@@ -0,0 +1,19 @@
+"""
+Clase pokebola. La idea de esta clase es definir todos los
+métodos propios de este objeto:
+    - Detectar colisión con el plano
+    - Modificar su velocidad
+    - Dibujarse
+
+@author Pablo Pizarro R. @ppizarror
+@date 2018
+@license MIT
+"""
+
+
+class Pokebola:
+    def __init__(self):
+        """
+        Constructor
+        """
+        pass
